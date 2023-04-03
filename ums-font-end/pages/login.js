295dcfbe298921e350d219637fee3ee3da73fe45
Md. Fahim Alam <76841119+Fahim2280@users.noreply.github.com>
2023-04-04 03:04:45 +0600

    Sing in and Sing Up

---
+++ ums-font-end/pages/login.js
@@ -0,0 +1,21 @@
+export default function Login() {
+  return (
+    <>
+      <h2>Sing Up</h2>
+      <form>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      <a href="register">
+        <button>Sing Up</button>
+      </a>
+    </>
+  );
+}
