56f718968be4db3092374034e77504157cdcbcb2
SrTamim <istiakmahmud.aiub@gmail.com>
2023-05-09 13:03:46 +0600

    get css update

--- ums-font-end/pages/admin/dashboard/updatestudent.js
+++ ums-font-end/pages/admin/dashboard/updatestudent.js
@@ -222,7 +222,7 @@ export default function UpdateStudent() {
           onClick={() => router.back()}
           class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          {" "}
+          
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="100%"
