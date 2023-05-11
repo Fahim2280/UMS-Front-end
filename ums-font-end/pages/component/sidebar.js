252a15bcd33780533bcb584cd0861356e1f4ede7
SrTamim <istiakmahmud.aiub@gmail.com>
2023-05-12 03:58:55 +0600

    add officer

--- ums-font-end/pages/component/sidebar.js
+++ ums-font-end/pages/component/sidebar.js
@@ -95,7 +95,7 @@ export default function SideBar() {
 
             <li>
               <a
-                href="/admin/dashboard/addcourse"
+                href="/admin/dashboard/addofficer"
                 class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
               >
                 <svg
