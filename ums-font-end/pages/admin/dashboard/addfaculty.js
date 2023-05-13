07d34306611e968ed238ca34d559990ef181b2cd
SrTamim <istiakmahmud.aiub@gmail.com>
2023-05-14 02:19:58 +0600

    course, faculty validation

--- ums-font-end/pages/admin/dashboard/addfaculty.js
+++ ums-font-end/pages/admin/dashboard/addfaculty.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import MyLayout from "@/pages/component/layout";
 import { useRouter } from "next/router";
 import SessionCheck from "../../component/sessioncheck";
-import SideBar from "../../../pages/component/sidebar"; 
+import SideBar from "../../../pages/component/sidebar";
 import Footer from "../../../pages/component/footer";
 
 export default function AddFaculty() {
@@ -17,8 +17,47 @@ export default function AddFaculty() {
     Fdob: "",
   });
 
+  const [errors, setErrors] = useState({});
+
+  const validateForm = () => {
+    let errors = {};
+
+    if (!formData.Fidd) {
+      errors.Fidd = "id is required";
+    }
+
+    if (!formData.Fname) {
+      errors.Fname = "name is required";
+    }
+
+    if (!formData.Fprogram) {
+      errors.Fprogram = "program is required";
+    }
+
+    if (!formData.Fdep) {
+      errors.Fdep = "depertment is required";
+    }
+    if (!formData.Faddress) {
+      errors.Faddress = "address is required";
+    }
+    if (!formData.Fnum) {
+      errors.Fnum = "number is required";
+    }
+    if (!formData.Fdob) {
+      errors.Fdob = "date of birth is required";
+    }
+
+    return errors;
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+    const errors = validateForm();
+    if (Object.keys(errors).length > 0) {
+      setErrors(errors);
+      return;
+    }
 
     const response = await fetch("http://localhost:3000/admin/insertfaculty/", {
       method: "POST",
@@ -42,6 +81,7 @@ export default function AddFaculty() {
       Fnum: "",
       Fdob: "",
     });
+    setErrors({});
   };
 
   const handleChange = (event) => {
@@ -54,113 +94,134 @@ export default function AddFaculty() {
 
   return (
     <>
-    <SessionCheck />
-    <div>
-    <SideBar />
-      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-10">
-        {/* heading */}
-        <h1 class="mb-9 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-1xl lg:text-1xl dark:text-white">
-          Add
-          <mark class="px-2 text-white bg-blue-600 rounded dark:bg-blue-500">
-            Faculty
-          </mark>
-        </h1>
-        {/* heading end */}
-        <form onSubmit={handleSubmit}>
-          <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Faculty id:
-            <input
-              type="text"
-              name="Fidd"
-              value={formData.Fidd}
-              onChange={handleChange}
-              class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs 
+      <SessionCheck />
+      <div>
+        <SideBar />
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-10">
+          {/* heading */}
+          <h1 class="mb-9 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-1xl lg:text-1xl dark:text-white">
+            Add
+            <mark class="px-2 text-white bg-blue-600 rounded dark:bg-blue-500">
+              Faculty
+            </mark>
+          </h1>
+          {/* heading end */}
+          <form onSubmit={handleSubmit}>
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Faculty id:
+              <input
+                type="text"
+                name="Fidd"
+                value={formData.Fidd}
+                onChange={handleChange}
+                class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs 
           focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
           dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            />
-          </label>
-          <br />
-          <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Faculty name:
-            <input
-              type="text"
-              name="Fname"
-              value={formData.Fname}
-              onChange={handleChange}
-              class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs 
+              />
+              {errors.Fidd && (
+                <div class="text-red-500 text-sm">{errors.Fidd}</div>
+              )}
+            </label>
+            <br />
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Faculty name:
+              <input
+                type="text"
+                name="Fname"
+                value={formData.Fname}
+                onChange={handleChange}
+                class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs 
           focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
           dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            />
-          </label>
-          <br />
-          <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Faculty:
-            <input
-              type="text"
-              name="Fprogram"
-              value={formData.Fprogram}
-              onChange={handleChange}
-              class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs 
+              />
+              {errors.Fname && (
+                <div class="text-red-500 text-sm">{errors.Fname}</div>
+              )}
+            </label>
+            <br />
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Faculty:
+              <input
+                type="text"
+                name="Fprogram"
+                value={formData.Fprogram}
+                onChange={handleChange}
+                class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs 
           focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
           dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            />
-          </label>
-          <br />
-          <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Depertment:
-            <input
-              type="text"
-              name="Fdep"
-              value={formData.Fdep}
-              onChange={handleChange}
-              class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs 
+              />
+              {errors.Fidd && (
+                <div class="text-red-500 text-sm">{errors.Fidd}</div>
+              )}
+            </label>
+            <br />
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Depertment:
+              <input
+                type="text"
+                name="Fdep"
+                value={formData.Fdep}
+                onChange={handleChange}
+                class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs 
           focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
           dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            />
-          </label>
-          <br />
-          <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            address:
-            <input
-              type="text"
-              name="Faddress"
-              value={formData.Faddress}
-              onChange={handleChange}
-              class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs 
+              />
+              {errors.Fdep && (
+                <div class="text-red-500 text-sm">{errors.Fdep}</div>
+              )}
+            </label>
+            <br />
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              address:
+              <input
+                type="text"
+                name="Faddress"
+                value={formData.Faddress}
+                onChange={handleChange}
+                class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs 
           focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
           dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            />
-          </label>
-          <br />
-          <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Phone Number:
-            <input
-              type="text"
-              name="Fnum"
-              value={formData.Fnum}
-              onChange={handleChange}
-              class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs 
+              />
+              {errors.Faddress && (
+                <div class="text-red-500 text-sm">{errors.Faddress}</div>
+              )}
+            </label>
+            <br />
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Phone Number:
+              <input
+                type="text"
+                name="Fnum"
+                value={formData.Fnum}
+                onChange={handleChange}
+                class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs 
           focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
           dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            />
-          </label>
-          <br />
-          <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Date of Birth:
-            <input
-              type="text"
-              name="Fdob"
-              value={formData.Fdob}
-              onChange={handleChange}
-              class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs 
+              />
+              {errors.Fnum && (
+                <div class="text-red-500 text-sm">{errors.Fnum}</div>
+              )}
+            </label>
+            <br />
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Date of Birth:
+              <input
+                type="text"
+                name="Fdob"
+                value={formData.Fdob}
+                onChange={handleChange}
+                class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs 
           focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
           dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            />
-          </label>
-          <br />
-      
-          {/* ...............submit button..................... */}
-          <button
+              />
+              {errors.Fdob && (
+                <div class="text-red-500 text-sm">{errors.Fdob}</div>
+              )}
+            </label>
+            <br />
+
+            {/* ...............submit button..................... */}
+            <button
               type="submit"
               class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
             >
@@ -169,12 +230,12 @@ export default function AddFaculty() {
               </span>
             </button>
             {/* ...............submit button end..................... */}
-        </form>
-        <br />
-    
-        {/* ...............back button..................... */}
-        {/* ...............back button..................... */}
-        <button
+          </form>
+          <br />
+
+          {/* ...............back button..................... */}
+          {/* ...............back button..................... */}
+          <button
             type="button"
             onClick={() => router.back()}
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -196,9 +257,9 @@ export default function AddFaculty() {
             <span class="sr-only">Icon description</span>
           </button>
           {/* ...............back button end..................... */}
+        </div>
       </div>
-    </div>
-    <Footer />
+      <Footer />
     </>
   );
 }
