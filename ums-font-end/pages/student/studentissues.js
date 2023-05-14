e84d1aca6b5655c9f2746f533321ecb79f1f832b
Asef-aiub <87447084+Asef-aiub@users.noreply.github.com>
2023-05-14 22:18:11 +0600

    relationship

---
+++ ums-font-end/pages/student/studentissues.js
@@ -0,0 +1,49 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+const StudentIssues = () => {
+  const [studentData, setStudentData] = useState(null);
+
+  async function fetchData() {
+    try {
+      const response = await fetch('http://localhost:3000/student/getstudentissue/1'); 
+      const data = await response.json();
+      setStudentData(data[0]);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {studentData ? (
+        <div>
+          <h1>{studentData.Sname}</h1>
+          <p>Department: {studentData.Sdep}</p>
+          <p>ID: {studentData.Sidd}</p>
+          <p>Address: {studentData.Saddress}</p>
+          <p>Phone: {studentData.Snum}</p>
+          <p>Program: {studentData.Sprogram}</p>
+          <h2>Issues:</h2>
+          <ul>
+            {studentData.issues.map((issue) => (
+              <li key={issue.Isid}>
+                <h3>{issue.issueType}</h3>
+                <p>{issue.issue}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <p>Loading data...</p>
+      )}
+    </div>
+  );
+};
+
+export default StudentIssues;
