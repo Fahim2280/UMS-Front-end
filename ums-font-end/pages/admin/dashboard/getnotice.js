24b8ea6190d390cb2b3cf52d9588947d3771292c
SrTamim <istiakmahmud.aiub@gmail.com>
2023-05-14 21:00:35 +0600

    get noti vali

--- ums-font-end/pages/admin/dashboard/getnotice.js
+++ ums-font-end/pages/admin/dashboard/getnotice.js
@@ -87,6 +87,7 @@ export default function GetNotice({ data }) {
             id="Nid"
             value={Nid}
             onChange={(e) => setNoticeId(e.target.value)}
+            required={true}
             class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs 
               focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
               dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
